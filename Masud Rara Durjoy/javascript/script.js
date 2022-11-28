22f32c5fee2290ad0d32e5d5ff52bf9a1734004c
MasudRana951 <118680879+MasudRana951@users.noreply.github.com>
2022-11-28 11:04:23 +0600

    figma project

---
+++ Masud Rara Durjoy/javascript/script.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function(){
+    $('.banner_slider').slick({
+        slidesToShow: 5,
+        arrows:false,
+        centerMode: true,
+        focusOnSelect: true,
+    });
+  });
